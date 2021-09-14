745a1337bde8a34614a4fd88fcab8d0ae3392ebc
Tim Morgner <timmorgner@gmail.com>
2021-09-14 20:04:59 +0200

    Kommentare

--- src/main/java/de/telekom/excelsior/DataInputController.java
+++ src/main/java/de/telekom/excelsior/DataInputController.java
@@ -6,30 +6,32 @@ import org.apache.poi.ss.usermodel.Sheet;
 import org.apache.poi.xssf.usermodel.XSSFWorkbook;
 import org.springframework.core.io.InputStreamResource;
 import org.springframework.http.MediaType;
-import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
-import javax.annotation.Resource;
 import java.io.*;
 
 @RestController
 public class DataInputController {
-    @PostMapping(produces = {MediaType.APPLICATION_OCTET_STREAM_VALUE},consumes = {MediaType.TEXT_PLAIN_VALUE})
+    @PostMapping(produces = {MediaType.APPLICATION_OCTET_STREAM_VALUE},consumes = {MediaType.TEXT_PLAIN_VALUE}) // Octet Stream, da wir einen byte Array zurück geben. Plain Text damit wir kein URL-Encoded Zeug bekommen.
     @ResponseBody
     InputStreamResource convert(@RequestBody String inputString) {
-        System.out.println("Input: "+inputString);
+        System.out.println("Input: "+inputString); //TODO Gescheites Logging
+
         XSSFWorkbook workbook = new XSSFWorkbook();
         Sheet sheet = workbook.createSheet();
         Row row = sheet.createRow(0);
         Cell cell = row.createCell(0);
         cell.setCellValue(inputString);
+
         ByteArrayOutputStream outputStream = new ByteArrayOutputStream();
         try {
             workbook.write(outputStream);
         } catch (IOException e) {
             e.printStackTrace();
+            return null;
         }
-        byte[] buffer = outputStream.toByteArray();
+
+        byte[] buffer = outputStream.toByteArray(); //Excel Dokument als byte Array
         ByteArrayInputStream inputStream = new ByteArrayInputStream(buffer);
         return new InputStreamResource(inputStream);
     }
